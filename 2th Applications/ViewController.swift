7e1886261b0f1fbec4c92a969d312ea2ee51ecaa
Fauzan18 <fauzanibnusarky@gmail.com>
2017-11-28 14:15:21 +0700

    07-Application
    07-App

--- 2th Applications/ViewController.swift
+++ 2th Applications/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputBil1: UITextField!
+    @IBOutlet weak var inputBil2: UITextField!
+    @IBOutlet weak var inputBil3: UITextField!
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func button(_ sender: Any) {
+        let bilangan1 : Int? = Int(inputBil1.text!)
+        let bilangan2 : Int? = Int(inputBil2.text!)
+        let bilangan3 : Int? = Int(inputBil3.text!)
+        
+        let Ratarata : Int = (bilangan1! + bilangan2! + bilangan3!)/3
+        label.text = "Your Average = \(Ratarata)"
     }
 
     override func didReceiveMemoryWarning() {
